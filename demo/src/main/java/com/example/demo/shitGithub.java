cd989193c443adb97a718e7fae262189da97f9ce
rkdansdlf <ansdlf000616e@gmail.com>
2025-09-16 12:02:59 +0900

    ㅈ같은 깃허브

--- demo/src/main/java/com/example/demo/shitGithub.java
+++ demo/src/main/java/com/example/demo/shitGithub.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 public class shitGithub {
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		System.out.println("Hello, World!"); 
 	}
 	
 }
